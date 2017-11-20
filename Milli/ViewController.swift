ac55fe952b97b29cbbe6785f9634b3b2f3cf911e
cvwang <charlesvwang@gmail.com>
2017-11-20 00:22:36 -0800

    Initial Commit

---
+++ Milli/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Milli
+//
+//  Created by Charles Wang on 11/20/17.
+//  Copyright © 2017 Milli. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
